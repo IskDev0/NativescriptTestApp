54337b6fbac4c6cc253dd4cff76c0423d23a72fb
Iskander <kubeev2001@gmail.com>
2022-11-26 15:41:08 +0600

    init

---
+++ app/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <Page>
+    <ActionBar>
+      <Label text="Test App"/>
+    </ActionBar>
+
+    <FlexboxLayout flexDirection="column">
+      <TextField v-model="textFieldValue" hint="Enter text..." />
+      <Label class="textField" :text="textFieldValue"></Label>
+    </FlexboxLayout>
+  </Page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 10,
+      textFieldValue: ""
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+@import '@nativescript/theme/scss/variables/blue';
+
+.textField{
+  font-size: 20px;
+}
+</style>
