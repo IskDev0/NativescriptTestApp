013e390b907f034cb8970d3eb6a72e3510441987
Iskander <kubeev2001@gmail.com>
2022-12-07 15:57:54 +0600

    ftyjghjfg

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -16,17 +16,19 @@
               @submit="fetchData"/>
           <ActivityIndicator v-if="isBusy" :busy="isBusy"/>
         </FlexboxLayout>
-        <StackLayout
-            class="text-white px-4 py-4"
-            v-for="film in this.films"
-            :key="film.imdbID">
-          <Image class="rounded-t-lg object-cover h-max w-full" :src="film.Poster"/>
-          <StackLayout class="bg-blue-400 text-white p-2">
-            <Label class="text-xl">{{ film.Title }}</Label>
-            <Label class="text-lg">{{ film.Year }}</Label>
-            <Label>{{ film.Type }}</Label>
-          </StackLayout>
-        </StackLayout>
+        <ListView for="film in this.films" @itemTap="onItemTap" style="height:20000px">
+          <v-template>
+            <FlexboxLayout class="py-4" flexDirection="column">
+              <Image class="rounded-t-lg object-cover h-max w-full" :src="film.Poster"/>
+              -->
+              <StackLayout class="bg-blue-400 text-white p-2">
+                <Label class="text-xl">{{ film.Title }}</Label>
+                <Label class="text-lg">{{ film.Year }}</Label>
+                <Label>{{ film.Type }}</Label>
+              </StackLayout>
+            </FlexboxLayout>
+          </v-template>
+        </ListView>
       </FlexboxLayout>
     </ScrollView>
   </Page>
@@ -66,7 +68,53 @@ export default {
         message: "This film does not exist",
         okButtonText: "OK"
       })
+    },
+    onItemTap: function (args) {
+      this.$navigateTo(details, {
+        context: {
+          propsData: {
+            imageSrc: args.item.Poster,
+            filmTitle: args.item.Title
+          }
+        }
+      })
+      console.log(args.item.Title + ' tapped');
+
+    },
+  },
+}
+
+
+const details = {
+  props: {
+    imageSrc: {
+      type: String,
+      required: true
+    },
+    filmTitle: {
+      type: String,
+      required: true
     }
   },
+  methods: {
+    goBack() {
+      this.$navigateBack()
+    },
+  },
+
+  template: `
+    <Page class="bg-slate-800 text-white">
+    <ActionBar class="bg-slate-800">
+      <Label text="Details"/>
+    </ActionBar>
+    <ScrollView>
+      <FlexboxLayout flexDirection="column">
+        <Image :src="this.imageSrc"/>
+        <Label :text="this.imageSrc"></Label>
+        <Button class="py-4 bg-indigo-700 text-white rounded-full" @tap="goBack">Back</Button>
+      </FlexboxLayout>
+    </ScrollView>
+    </Page>
+  `
 }
 </script>
